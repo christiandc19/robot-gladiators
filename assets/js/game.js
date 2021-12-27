e43888e6cc4edd0d59ef1688823d8b34843df807
unknown <christiandc19@hotmail.com>
2021-12-26 23:20:55 -0800

    add index.html and game.js file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-alert("this is an alert! Javascript is running")
+var playerName = window.prompt("What is your Robot's name?")
+console.log(playerName);
+
+var yourName = "Chris"
+
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("Our Robot's name is " + playerName);
+console.log(yourName);
+
+function fight() {
+    window.alert("The fight has begun");
+}
+// fight();
